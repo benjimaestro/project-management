ddfd813f640cb967772e43e55060f4898abe0e0c
p2408758 <p2408758@GWHXX472WIN012W>
2020-02-04 14:43:04 +0000

    Added testing and class for staff management

---
+++ TabarTesting/tstStaff1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TabarClasses;
+
+namespace TabarTesting
+{
+    [TestClass]
+    public class tstStaff1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //Test to see that it exists
+            Assert.IsNotNull(AnStaff);
+        }
+    }
+}
