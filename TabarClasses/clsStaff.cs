a79515657545353d1e60f5b9cd6604bc843149bf
p2408758 <p2408758@GWHXX472WIN012W>
2020-02-04 15:24:17 +0000

    Added more testing for staff management

--- TabarClasses/clsStaff.cs
+++ TabarClasses/clsStaff.cs
@@ -1,6 +1,17 @@
-﻿namespace TabarClasses
+﻿using System;
+
+namespace TabarClasses
 {
     public class clsStaff
     {
+        public bool Active { get; set; }
+        public DateTime DOB { get; set; }
+        public string PostCode { get; set; }
+        public Microsoft.VisualStudio.TestTools.UnitTesting.ITestDataSource HouseNo { get; set; }
+        public string Street { get; set; }
+        public string Town { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string TelephoneNo { get; set; }
     }
 }
