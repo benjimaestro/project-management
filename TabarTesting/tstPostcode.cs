ac4cfae89f051a95d94bddbbf95eb48bebd13252
Benjamin Jones <P2423891@my365.dmu.ac.uk>
2020-02-04 14:46:46 +0000

    Added testing and class for customer input validation

---
+++ TabarTesting/tstPostcode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using TabarClasses;
+
+namespace TabarTesting
+{
+    [TestClass]
+    public class tstPostcode
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsValidate Validator = new clsValidate();
+            Assert.IsNotNull(Validator);
+        }
+    }
+}
