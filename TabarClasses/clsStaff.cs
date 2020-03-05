f7b850c47849ee1fb34b060d4fc51bb1dac73432
benjimaestro <benjamin.gjones@yahoo.co.uk>
2020-03-05 01:06:09 +0000

    Fixed error

--- TabarClasses/clsStaff.cs
+++ TabarClasses/clsStaff.cs
@@ -7,7 +7,8 @@ namespace TabarClasses
         public bool Active { get; set; }
         public DateTime DOB { get; set; }
         public string PostCode { get; set; }
-        public Microsoft.VisualStudio.TestTools.UnitTesting.ITestDataSource HouseNo { get; set; }
+        //public Microsoft.VisualStudio.TestTools.UnitTesting.ITestDataSource HouseNo { get; set; }
+        public string HouseNo { get; set; }
         public string Street { get; set; }
         public string Town { get; set; }
         public string FirstName { get; set; }
